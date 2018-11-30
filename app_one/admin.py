3153ff149f3e110e86434a82a9f6377561f0d3a6
Fred Paucar <fred.paucarespinoza@gmail.com>
2018-11-30 17:53:53 -0500

    adding model

--- app_one/admin.py
+++ app_one/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from app_one.models import Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('title', 'created', 'slug')
+    search_fields = ('title', 'slug', 'body')
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+    prepopulated_fields = {'slug':('title',)}
+
+admin.site.register(Post, PostAdmin)
