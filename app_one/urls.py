7bfd84bdeab98648b46404b395e2ffc2cc4e6751
Fred Paucar <fred.paucarespinoza@gmail.com>
2019-02-11 18:50:22 -0500

    adding template base

--- app_one/urls.py
+++ app_one/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import home
+from .views import *
 
 urlpatterns = [
     url(r'home/', home),
+    url(r'posts/', post_list),
 ]
--- app_one/views.py
+++ app_one/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from app_one.models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'post/index.html')
+    return render(request, 'post/index.html',
+                  {'posts': Post.objects.count(),
+                  'dfg': 'Django'})
+
+def post_list(request):
+    # subs = Post._meta.model.objects.all()
+    # subs = Post.objects.all()
+    subs = Post.objects.filter(
+        status = 'published'
+    )
+    return render(request, 'post/index.html',
+                  {'subidos': subs})
+
+def post_list(request):
+    subs = Post.objects.filter(
+        status = 'draft'
+    )
+    return render(request, 'post/post_list.html',
+                  {'subidos': subs})
